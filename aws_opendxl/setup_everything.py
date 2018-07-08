0599a4bbca397331c9fbe30dc6f3e544c7325b6e
Amol Kokje <amolkokje@gmail.com>
2018-07-08 15:59:41 -0700

    some more debugging

--- aws_opendxl/setup_everything.py
+++ aws_opendxl/setup_everything.py
@@ -7,8 +7,13 @@ from moto import mock_s3, mock_ecs, mock_ec2, mock_cloudformation
 # SNS triggers Lambda
 # Lambda has DXL client which connects to the broker and sends event /dxl/test/event
 
-# 
-
+vpc_name = 'test_vpc'
+subnet_name = 'test_subnet'
+cluster_name = 'test_cluster'
+task_def_name = 'test_task_def'
+task_name = 'test_task'
+container_name = 'test_container'
+    
 @mock_s3
 @mock_ec2
 @mock_ecs
@@ -17,45 +22,74 @@ def main():
     s3_client = boto3.client('s3')
     ec2_client = boto3.client('ec2')
     ecs_client = boto3.client('ecs')
+    vpc_id, subnet_id = deploy_vpc_subnet(ec2_client)
+    print "VPC={}, SUBNET={}".format(vpc_id, subnet_id)
+    deploy_dxlbroker(ecs_client, vpc_id, subnet_id)
+
+
+def deploy_vpc_subnet(client):
+    vpc = client.create_vpc(
+		CidrBlock='10.0.0.0/8',
+		AmazonProvidedIpv6CidrBlock=False,
+		InstanceTenancy='default'
+	)	
+    print "VPC = {}".format(vpc)
+    vpc_id = vpc['Vpc']['VpcId']
+    subnet = client.create_subnet(CidrBlock='10.0.0.0/16', VpcId=vpc_id)
+    print "Subnet = {}".format(subnet)
+    subnet_id = subnet['Subnet']['SubnetId']
+    return vpc_id, subnet_id
     
-    deploy_dxlbroker(ecs_client)
-
-
-def deploy_dxlbroker(client):
+    
+def deploy_dxlbroker(client, vpc_id, subnet_id):
     print 'Deploying dxlbroker container ...'
-    cluster_name = 'test_cluster'
-    task_def_name = 'test_task_def'
-    task_name = 'test_task'
-            
     client.create_cluster(clusterName=cluster_name)
-    
-    client.register_task_definition(
-		family=task_def_name,
-		containerDefinitions=[
+    task_def = client.register_task_definition( 
+        family=task_def_name,
+        containerDefinitions=[
 			{
-				'name': 'test_container',
-				'image': 'amolkokje/opendxl-environment-centos',
-				'portMappings': [
-					{
-						'containerPort': 8000,
-						'hostPort': 5000,
-						'protocol': 'tcp'
-					},
-				],
-				'hostname': 'amoltest',
-				'user': 'amol',
-				'privileged': True,
-				'readonlyRootFilesystem': False,            
-				
-			},
-		],
-		requiresCompatibilities=[
-			'FARGATE',
-		]
-	)
-	##
-	print client.list_task_definitions()
+                'name': container_name,
+                'image': 'amolkokje/opendxl-environment-centos',
+                'portMappings': [
+                    {
+                        'containerPort': 8000,
+                        'hostPort': 5000,
+                        'protocol': 'tcp'
+                    },
+                ],
+                #'hostname': 'amoltest',
+                'user': 'amol',
+                'privileged': True,
+                'readonlyRootFilesystem': False
+            }   
+        ],
+        requiresCompatibilities=[
+            'FARGATE'
+        ]
+    )
+    print "Task Definition = {}".format(task_def)
+    print "Task definitions = {}".format(client.list_task_definitions())
+	
+    """
+    response = client.run_task(
+		cluster=cluster_name,
+		taskDefinition='{}:1'.format(task_def_name),
+		count=1,
+		launchType='FARGATE',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [
+                    subnet_id
+                ]
+            }
+        }
+    )
+    print "Response = {}".format(response)	
+    print "Tasks = {}".format(client.list_tasks())
+	"""
     
+    print "Deploying dxlbroker done."
+	
 
 if __name__ == '__main__':
     main()
---
+++ aws_opendxl/setup_example.py
@@ -0,0 +1,170 @@
+import boto3
+import pprint
+from moto import mock_s3, mock_ecs, mock_ec2, mock_cloudformation
+
+# ECS Details
+cluster_name = "BotoCluster"
+service_name = "service_hello_world"
+task_name = "hello_world"
+
+ecs_client = boto3.client('ecs')
+ec2_client = boto3.client('ec2')
+
+def launch_ecs_example():
+    response = ecs_client.create_cluster(
+        clusterName=cluster_name
+    )
+    pprint.pprint(response)
+
+    # Create EC2 instance(s) in the cluster
+    # For now I expect a default cluster to be there
+
+    # By default, your container instance launches into your default cluster.
+    # If you want to launch into your own cluster instead of the default,
+    # choose the Advanced Details list and paste the following script
+    # into the User data field, replacing your_cluster_name with the name of your cluster.
+    # !/bin/bash
+    # echo ECS_CLUSTER=your_cluster_name >> /etc/ecs/ecs.config
+
+    response = ec2_client.run_instances(
+        # Use the official ECS image
+        ImageId="ami-a98cb2c3",
+        MinCount=1,
+        MaxCount=1,
+        InstanceType="t2.micro",
+        IamInstanceProfile={
+            "Name": "ecsInstanceRole"
+        },
+        UserData="#!/bin/bash \n echo ECS_CLUSTER=" + cluster_name + " >> /etc/ecs/ecs.config"
+    )
+    pprint.pprint(response)
+
+    # Create a task definition
+    response = ecs_client.register_task_definition(
+        containerDefinitions=[
+        {
+          "name": "wordpress",
+          "links": [
+            "mysql"
+          ],
+          "image": "wordpress",
+          "essential": True,
+          "portMappings": [
+            {
+              "containerPort": 80,
+              "hostPort": 80
+            }
+          ],
+          "memory": 300,
+          "cpu": 10
+        },
+        {
+          "environment": [
+            {
+              "name": "MYSQL_ROOT_PASSWORD",
+              "value": "password"
+            }
+          ],
+          "name": "mysql",
+          "image": "mysql",
+          "cpu": 10,
+          "memory": 300,
+          "essential": True
+        }
+        ],
+        family="hello_world"
+    )
+    pprint.pprint(response)
+
+    # Create service with exactly 1 desired instance of the task
+    # Info: Amazon ECS allows you to run and maintain a specified number
+    # (the "desired count") of instances of a task definition
+    # simultaneously in an ECS cluster.
+
+    response = ecs_client.create_service(
+        cluster=cluster_name,
+        serviceName=service_name,
+        taskDefinition=task_name,
+        desiredCount=1,
+        clientToken='request_identifier_string',
+        deploymentConfiguration={
+            'maximumPercent': 200,
+            'minimumHealthyPercent': 50
+        }
+    )
+    pprint.pprint(response)
+    
+    # list everything
+    #ecs_client.list_services()
+    #print "TASKS = {}".format(ecs_client.list_tasks())
+    #print "CONTAINER INSTANCES = {}".format(ecs_client.list_container_instances())
+    
+
+# Shut everything down and delete task/service/instance/cluster
+def terminate_ecs_example():
+    try:
+        # Set desired service count to 0 (obligatory to delete)
+        response = ecs_client.update_service(
+            cluster=cluster_name,
+            service=service_name,
+            desiredCount=0
+        )
+        # Delete service
+        response = ecs_client.delete_service(
+            cluster=cluster_name,
+            service=service_name
+        )
+        pprint.pprint(response)
+    except:
+        print("Service not found/not active")
+
+    # List all task definitions and revisions
+    response = ecs_client.list_task_definitions(
+        familyPrefix=task_name,
+        status='ACTIVE'
+    )
+
+    # De-Register all task definitions
+    for task_definition in response["taskDefinitionArns"]:
+        # De-register task definition(s)
+        deregister_response = ecs_client.deregister_task_definition(
+            taskDefinition=task_definition
+        )
+        pprint.pprint(deregister_response)
+
+    # Terminate virtual machine(s)
+    response = ecs_client.list_container_instances(
+        cluster=cluster_name
+    )
+    if response["containerInstanceArns"]:
+        container_instance_resp = ecs_client.describe_container_instances(
+            cluster=cluster_name,
+            containerInstances=response["containerInstanceArns"]
+        )
+        for ec2_instance in container_instance_resp["containerInstances"]:
+            ec2_termination_resp = ec2_client.terminate_instances(
+                DryRun=False,
+                InstanceIds=[
+                    ec2_instance["ec2InstanceId"],
+                ]
+            )
+
+    # Finally delete the cluster
+    response = ecs_client.delete_cluster(
+        cluster=cluster_name
+    )
+    pprint.pprint(response)
+
+    
+    
+@mock_s3
+@mock_ec2
+@mock_ecs
+@mock_cloudformation    
+def main():
+    launch_ecs_example()
+    #terminate_ecs_example()
+    
+    
+if __name__ == '__main__':
+    main()
