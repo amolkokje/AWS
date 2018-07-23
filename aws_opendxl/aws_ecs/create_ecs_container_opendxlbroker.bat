85021cebafb7998c4e6779106f4b102de0732f6e
Amol Kokje <amolkokje@gmail.com>
2018-07-23 01:07:56 -0700

    added a comment

--- aws_opendxl/aws_ecs/create_ecs_container_opendxlbroker.bat
+++ aws_opendxl/aws_ecs/create_ecs_container_opendxlbroker.bat
@@ -1,4 +1,7 @@
-setlocal
+:: setlocal
+
+:: option-1, run task def directly
+:: option-2, run task def from a service
 
 set TASKDEFJSON=C:\Users\aakokje\amol-opendxlbroker-task-def.json
 :: this name should be in the json file as 'family'
@@ -19,9 +22,9 @@ aws ecs create-cluster --cluster-name %CLUSTERNAME%
 aws ecs register-task-definition --cli-input-json file://%TASKDEFJSON%
 aws ecs list-task-definitions
 
-aws ecs create-service --cluster %CLUSTERNAME% --service-name %SERVICENAME% --task-definition %TASKDEFNAME% --desired-count %DESIREDTASKCOUNT% --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[%SUBNET%],securityGroups=[%SECURITYGROUP%]}"
+aws ecs create-service --cluster %CLUSTERNAME% --service-name %SERVICENAME% --task-definition %TASKDEFNAME% --desired-count %DESIREDTASKCOUNT% --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[%SUBNET%],securityGroups=[%SECURITYGROUP%],assignPublicIp=ENABLED}"
 aws ecs list-services --cluster %CLUSTERNAME%
 
 aws ecs describe-services --cluster %CLUSTERNAME% --services %SERVICENAME%
 
-endlocal
+:: endlocal
